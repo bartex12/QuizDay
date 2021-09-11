82b31c0a6d84f4bd0ad1bb7d398e782cfd342eed
bartex12 <37575336+bartex12@users.noreply.github.com>
2021-09-12 00:01:54 +0300

    диалог результатов - поменял макет

--- app/src/main/java/com/bartex/quizday/ui/flags/base/BaseResultDialog.kt
+++ app/src/main/java/com/bartex/quizday/ui/flags/base/BaseResultDialog.kt
@@ -3,6 +3,8 @@ package com.bartex.quizday.ui.flags.base
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
@@ -28,14 +30,23 @@ abstract class BaseResultDialog: DialogFragment(){
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val builder = AlertDialog.Builder(activity )
-        builder.setMessage(
-                getString(R.string.results, total, totalGuesses, total*100/totalGuesses.toDouble())
-        )
-        builder.setPositiveButton(R.string.reset_quiz) { _, _ ->
+        val inflater = requireActivity().layoutInflater
+        val view: View = inflater.inflate(R.layout.dialog_statistica, null)
+        builder.setView(view)
+
+        view.findViewById<TextView>(R.id.results).text = getString(R.string.result)
+            view.findViewById<TextView>(R.id.questions).text = getString(R.string.results_questions, total)
+        view.findViewById<TextView>(R.id.attempts).text = getString(R.string.results_attempts, totalGuesses)
+        view.findViewById<TextView>(R.id.performance).text =
+            getString(R.string.results_performance, total*100/totalGuesses.toDouble())
+
+        view.findViewById<TextView>(R.id.button_ok).setOnClickListener {
             getCurrentViewModel().resetQuiz() //просто вызываем метод вьюмодели
+            dismiss()
         }
         return builder.create().apply {
             setCanceledOnTouchOutside(false)
         }
     }
+    
 }
