9c57c92c752e2840a4dbd071a2059edba894b8e3
bartex12 <37575336+bartex12@users.noreply.github.com>
2021-09-13 17:31:41 +0300

    Миграция в отдельном классе

--- app/src/main/java/com/bartex/quizday/room/Database.kt
+++ app/src/main/java/com/bartex/quizday/room/Database.kt
@@ -3,10 +3,8 @@ package com.bartex.quizday.room
 import android.content.Context
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.room.migration.Migration
-import androidx.sqlite.db.SupportSQLiteDatabase
 import com.bartex.quizday.room.dao.StateDao
-import com.bartex.quizday.room.tables.MigrationFromDb
+import com.bartex.quizday.room.tables.MigrationDb
 import com.bartex.quizday.room.tables.RoomState
 
 @androidx.room.Database(entities = [RoomState::class], version = 2)
@@ -24,7 +22,7 @@ abstract class Database : RoomDatabase() {
         fun create(context:Context) {
            if(instance == null) {
                instance = Room.databaseBuilder(context, Database::class.java, DB_NAME )
-                   .addMigrations(MigrationFromDb().migration1to2)
+                   .addMigrations(MigrationDb().migration1to2)
                    .build()
            }
         }
--- app/src/main/java/com/bartex/quizday/room/tables/MigrationDb.kt
+++ app/src/main/java/com/bartex/quizday/room/tables/MigrationDb.kt
@@ -3,7 +3,7 @@ package com.bartex.quizday.room.tables
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
 
-class MigrationFromDb {
+class MigrationDb {
    val migration1to2 = object : Migration(1,2){
         override fun migrate(database: SupportSQLiteDatabase) {
             database.execSQL("ALTER TABLE RoomState ADD COLUMN mistake INTEGER DEFAULT 0  NOT NULL")
